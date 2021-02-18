83d6128547edecfb809c14bb399ff1b5c89422e6
Eduardo Azevedo <eduardoazvd17@gmail.com>
2021-02-18 18:17:29 -0300

    Creating testing classes

---
+++ test/app/modules/movie/movie_controller_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:the_movie_challenge/app/modules/movie/stores/movie_controller.dart';
+import 'package:the_movie_challenge/app/modules/movie/movie_module.dart';
+
+void main() {
+  initModule(MovieModule());
+  // MovieController movie;
+  //
+  setUp(() {
+    //     movie = MovieModule.to.get<MovieController>();
+  });
+
+  group('MovieController Test', () {
+    //   test("First Test", () {
+    //     expect(movie, isInstanceOf<MovieController>());
+    //   });
+
+    //   test("Set Value", () {
+    //     expect(movie.value, equals(0));
+    //     movie.increment();
+    //     expect(movie.value, equals(1));
+    //   });
+  });
+}
---
+++ test/app/modules/movie/movie_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_modular/flutter_modular_test.dart';
+
+import 'package:the_movie_challenge/app/modules/movie/pages/movie_page.dart';
+
+void main() {
+  testWidgets('MoviePage has title', (tester) async {
+    //  await tester.pumpWidget(buildTestableWidget(MoviePage(title: 'Movie')));
+    //  final titleFinder = find.text('Movie');
+    //  expect(titleFinder, findsOneWidget);
+  });
+}
